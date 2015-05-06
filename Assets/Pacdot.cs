752d9d527411a473d24e280a51caebebd411d69e
faizan101 <faizan2019@gmail.com>
2015-05-06 10:19:01 +0500

    Score Functionality added!

--- Assets/Pacdot.cs
+++ Assets/Pacdot.cs
@@ -1,21 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Pacdot : MonoBehaviour {
-	public static int count=0;
-	//temp obj = new temp ();
+	public static float score=0.0f;
+
 	void OnTriggerEnter2D (Collider2D other)
 	{
 
 		if (other.name == "pacman") 
 		{
-
-			count++;
-			Debug.Log(count);
+			score++;
+			Debug.Log(score);
 			Destroy (gameObject);
+			//AddScore(score);
 
 		}
 
 	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect (260, 12, 120, 50), "Score:");
+		GUI.Label (new Rect (310, 12, 120, 50), score.ToString ());
+	}
+	/*void AddScore(int newScore)
+	{
+		score += newScore;
+		UpdateScore ();
+	}
+	void UpdateScore()
+	{
+
+		scoreText.text = "Score: " + score;
+	}*/
 }
 
