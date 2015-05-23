a2357ded3b33d1312452768a407de66da40d0a4b
faizan101 <faizan2019@gmail.com>
2015-05-23 16:40:05 +0500

    GameOver Screen and Functionality Added

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void OnGUI () {
+		
+		if(GUI.Button(new Rect(600,170,120,30), "Restart Level")) {
+			Application.LoadLevel("Pacman1");
+		}
+		if(GUI.Button(new Rect(600,210,120,30), "MainMenu")) {
+			Application.LoadLevel("MainMenu");
+		}
+		if(GUI.Button(new Rect(600,250,120,30), "Exit")) {
+			Application.Quit();
+		}
+	}
+}
