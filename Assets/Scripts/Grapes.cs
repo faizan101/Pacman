e5432ec6a085b5ed2c48d977745fa5da866c211d
faizan101 <faizan2019@gmail.com>
2015-05-26 19:45:42 +0500

    Fruit added

---
+++ Assets/Scripts/Grapes.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grapes : MonoBehaviour {
+	
+	// Use this for initialization
+	public GameObject[] enemies;
+	EnemyAI temp;
+	GhostMovement temp1;
+	
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.name == "pacman") {
+			Vector2 a=transform.position;
+			a.x=100; a.y=100;
+			transform.position=a;
+			StartCoroutine(Slow());
+		}
+	}
+	
+	IEnumerator Slow()
+	{
+		//Debug.LogError ("No Grape found? PANIC!");
+		temp = enemies[0].GetComponent<EnemyAI>();
+		temp.speed = 50f;
+		temp.speed1 = 0.1f;
+		temp = enemies[1].GetComponent<EnemyAI>();
+		temp.speed = 50f;
+		temp.speed1 = 0.1f;
+		temp1 = enemies[2].GetComponent<GhostMovement>();
+		temp1.speed = 0.1f;
+		temp1 = enemies[3].GetComponent<GhostMovement>();
+		temp1.speed = 0.1f;
+		Debug.Log (temp.speed);
+		yield return new WaitForSeconds(7.0f);
+		
+		temp = enemies[0].GetComponent<EnemyAI>();
+		temp.speed = 100f;
+		temp.speed1 = 0.2f;
+		temp = enemies[1].GetComponent<EnemyAI>();
+		temp.speed = 100f;
+		temp.speed1 = 0.2f;
+		temp1 = enemies[2].GetComponent<GhostMovement>();
+		temp1.speed = 0.2f;
+		temp1 = enemies[3].GetComponent<GhostMovement>();
+		temp1.speed = 0.2f;
+		Destroy (gameObject);
+	}
+}
